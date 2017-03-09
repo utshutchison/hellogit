3ecb955c654a727fbc2cc05eb693cd164ef8984c
Grant Hutchison <grant.hutchison@utschools.ca>
2017-03-09 15:08:00 -0500

    initial

---
+++ TestHobbits/src/Hobbit.java
@@ -0,0 +1,12 @@
+
+public class Hobbit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] hobbits = {"Bilbo","Frodo"};
+		for (int i=0; i<hobbits.length; i++){
+			System.out.println(hobbits[i]);
+		}
+	}
+
+}
